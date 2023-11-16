08714746914f956045eed3d7d530191b6033770a
vivekkum-ar <vivekkr9119@gmail.com>
2023-11-16 10:14:29 +0530

    Initial commit with complete navbar

---
+++ components/main-nav.tsx
@@ -0,0 +1,152 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, components, navigationMenuTriggerStyle } from "./ui/navigation-menu";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Icon } from '@iconify/react';
+
+interface NavbarProps {
+    // Add your prop types here
+}
+const ListItem = React.forwardRef<
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <a
+                    ref={ref}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-sm font-medium leading-none">{title}</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {children}
+                    </p>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    )
+})
+ListItem.displayName = "ListItem"
+
+const Navbar: React.FC<NavbarProps> = ({ }) => {
+
+    const [Theme, setTheme] = useState("");
+    useEffect(() => {
+        // Change the icons inside the button based on previous settings
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+            setTheme("dark");
+        } else {
+            document.documentElement.classList.remove('dark');
+            setTheme("light");
+        }
+    }, []);
+
+    function themeToggle() {
+        // if set via local storage previously
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+                setTheme("dark");
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+                setTheme("light");
+            }
+        }
+        // if NOT set via local storage previously
+        else {
+            if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+                setTheme("light");
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+                setTheme("dark");
+            }
+        }
+        console.log(document.documentElement.classList.contains('dark'));
+    };
+
+    
+    return (
+        <div className="px-4 md:px-6 lg:px-8 xl:px-10 flex-row-reverse flex justify-between py-2 bg-gradient-to-b dark:from-gray-600  dark:to-transparent from-gray-300  to-transparent">
+            <Button className="ms-1" variant={"outline"} size={"icon"} onClick={() => { themeToggle() }}>
+                <svg className={`${Theme === "dark" ? "" : "hidden"} w-5 h-5`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                <svg className={`${Theme === "light" ? "" : "hidden"} w-5 h-5`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+            </Button>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                                <li className="row-span-3">
+                                    <NavigationMenuLink asChild>
+                                        <a
+                                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                            href="/"
+                                        >
+                                            <Icon icon="mdi:weather-partly-lightning" fontSize={80} style={{color: "white"}} />
+                                            <div className="mb-2 mt-4 text-lg font-medium">
+                                                Weather Forecast App
+                                            </div>
+                                            <p className="text-sm leading-tight text-muted-foreground">
+                                                Get the latest weather updates for your location.
+                                            </p>
+                                        </a>
+                                    </NavigationMenuLink>
+                                </li>
+                                <ListItem href="/docs" title="Introduction">
+                                    Re-usable components built using Radix UI and Tailwind CSS.
+                                </ListItem>
+                                <ListItem href="/docs/installation" title="Installation">
+                                    How to install dependencies and structure your app.
+                                </ListItem>
+                                <ListItem href="/docs/primitives/typography" title="Typography">
+                                    Styles for headings, paragraphs, lists...etc
+                                </ListItem>
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                {components.map((component) => (
+                                    <ListItem
+                                    key={component.title}
+                                        title={component.title}
+                                        href={component.href}
+                                        >
+                                        {component.description}
+                                    </ListItem>
+                                ))}
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Documentation
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+<span className="flex flex-row items-center font-bold"><Icon icon="mdi:weather-partly-lightning"  style={{color: `${Theme == "dark" ? "white" : "black"}`}} fontSize={40}/> Weather App </span>
+        </div>
+    )
+}
+
+export default Navbar
