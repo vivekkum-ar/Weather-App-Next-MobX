140a7c1a4afb61e0851c19df8c5617cca1eb7b70
vivekkum-ar <vivekkr9119@gmail.com>
2023-11-18 18:37:21 +0530

    suggestion works here

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const Home = observer(() => {
   const mStore = mainStore;
 
   // useEffect(() => {
-  //   const fetchPosts = async () => {
+  //   const searchResults = async () => {
   //     try {
   //       if (mainStore.searchTerm === '' || mainStore.searchTerm !== undefined || mainStore.searchTerm !== null) {
   //         var response = await fetch(`/api/main?term=${""}&page=2`);
@@ -32,24 +32,24 @@ const Home = observer(() => {
   //     }
   //   };
 
-  //   fetchPosts();
+  //   searchResults();
   // }, [mainStore.searchTerm, page]);
-const [inputTxt, setinputTxt] = useState("");
-const [resultData, setresultData] = useState([]);
+  const [inputTxt, setinputTxt] = useState("");
+  const [resultData, setresultData] = useState([]);
 
-// Remove duplicates from the array
-function onlyUnique(value:string, index:number, array:string[]) {
-  return array.indexOf(value) === index;
-}
+  // Remove duplicates from the array
+  function onlyUnique(value: string, index: number, array: string[]) {
+    return array.indexOf(value) === index;
+  }
   useEffect(() => {
-    const fetchPosts = async () => {
+    const searchResults = async () => {
 
       try {
         // if (mainStore.searchTerm === '' || mainStore.searchTerm !== undefined || mainStore.searchTerm !== null) {
-          var response = await fetch(`/api/search?term=${inputTxt}&limit=5`);
+        var response = await fetch(`/api/search?term=${inputTxt}&limit=5`);
         // }
         // else {
-          // var response = await fetch(`/api/main?term=${mainStore.searchTerm}&page=2`);
+        // var response = await fetch(`/api/main?term=${mainStore.searchTerm}&page=2`);
         // }
         if (!response.ok) {
           throw new Error('Failed to fetch data');
@@ -65,7 +65,7 @@ function onlyUnique(value:string, index:number, array:string[]) {
       }
     };
 
-    fetchPosts();
+    searchResults();
   }, [inputTxt]);
   return (
     <div className='max-w-screen-xl mx-auto'>
@@ -73,17 +73,15 @@ function onlyUnique(value:string, index:number, array:string[]) {
       <div className="max-w-md mx-auto flex flex-row justify-center">
         <div className="absolute">
           <div className="flex flex-col justify-center items-center w-full">
-            <Button size={"icon"} className="absolute top-0 right-0 m-1 rounded-full"> <Icon fontSize={20} icon="flowbite:search-solid" style={{}} /></Button>
-            <input type="search" name="search" id="" onChange={(e) => {setinputTxt(e.target.value);}} className="w-60 p-3 rounded-full outline-2 peer outline-gray-400 shadow-xl text-md" placeholder="Enter city name..." />
+            <Button size={"icon"} className="absolute top-0 right-0 m-1 rounded-full shadow-lg"> <Icon fontSize={20} icon="flowbite:search-solid" style={{}} /></Button>
+            <input type="search"  value={inputTxt} name="search" id="" onChange={(e) => { setinputTxt(e.target.value); }} className="w-60 p-3 rounded-full outline-2 peer outline-gray-400 shadow-xl text-md" placeholder="Enter city name..." />
             <ul className="pt-1 peer-focus:visible invisible">
               {/* <li className="bg-gray-200 text-sm w-60 px-2 py-1 border-b-2 border-gray-300 hover:bg-blue-50 rounded-t-lg">1</li> */}
-              {
-              resultData.filter(onlyUnique).slice(0,5).map((item, index) => {
-                return(
-                  <li key={index} className="bg-gray-200 text-sm w-60 px-2 py-1 border-b-2 border-gray-300 hover:bg-blue-50">{item}</li>
+              {resultData.filter(onlyUnique).slice(0, 5).map((item, index) => {
+                return (
+                  <li onMouseDown={() => {setinputTxt(`${item}`);}} key={index} className={`cursor-pointer bg-gray-200 text-sm w-60 px-2 py-1 border-b-2 border-gray-300 hover:bg-blue-50 ${index == 0 ? "rounded-t-lg" : index >= 4 ? "rounded-b-lg" : ""}`}>{item}</li>
                 );
-              })
-              }
+              })}
               {/* <li className="bg-gray-200 text-sm w-60 px-2 py-1 border-b-2 border-gray-300 hover:bg-blue-50  rounded-b-lg">3</li> */}
             </ul>
           </div>
